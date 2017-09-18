502a25b697ec772bb00e57611c9bde228850c326
jmartins3 <jmartins@isel.pt>
2017-09-18 12:35:03 +0100

    Aula de 18/9

--- DotNet/Aula_2017_09_14/CtxSwitchTime.cs
+++ DotNet/Aula_2017_09_14/CtxSwitchTime.cs
@@ -7,26 +7,36 @@ namespace Aula_2017_09_14
     class CtxSwitchTime
     {
         private static readonly int N_ITERS = 1000000;
+
+        /// <summary>
+        /// code executed by test threads
+        /// </summary>
+        private void ThreadFunc()
+        {
+            for (int i = 0; i < N_ITERS; ++i)
+                Thread.Yield();
+        }
+
         public void Test()
         {
+            // force same CPU (1) for test threads
             Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
-            Thread t1 = new Thread(() =>
-            {
-                for (int i = 0; i < N_ITERS; ++i)
-                    Thread.Yield();
-            });
 
-            Thread t2 = new Thread(() =>
-            {
-                for (int i = 0; i < N_ITERS; ++i)
-                    Thread.Yield();
-            });
+            
 
-           
+            // Create 2 threads for test
+            Thread t1 = new Thread(ThreadFunc) ;
+            Thread t2 = new Thread(ThreadFunc);
+
+            // Force thread's priority to Highest
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.Highest;
+
             Stopwatch sw = Stopwatch.StartNew();
             t1.Start();
             t2.Start();
 
+            // Wait for thread termination
             t1.Join();
             t2.Join();
 
